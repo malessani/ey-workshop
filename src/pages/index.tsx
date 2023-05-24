f03f51a2797e2761ab2db88b6852d637fde4f42f
Matteo Alessani <matteo@commercelayer.io>
2023-05-24 18:55:45 +0200

    feat: add product list page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
-import type { HeadFC } from "gatsby";
+import { HeadFC, Link, PageProps, graphql, useStaticQuery } from "gatsby";
 import Page from "../components/Page";
-
 const IndexPage = () => {
+  const data = useStaticQuery<Queries.ProductsPageQuery>(graphql`
+    query ProductsPage {
+      allProductsJson {
+        edges {
+          node {
+            name
+            slug
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <Page title={"Gatsby Workshop"}>
       <p>Starting the workshop now</p>
+      <ul>
+        {data.allProductsJson.edges.map(({ node }) => (
+          <li key={node.name}>
+            {node.slug && <Link to={node.slug}>{node.name}</Link>}
+          </li>
+        ))}
+      </ul>
     </Page>
   );
 };
