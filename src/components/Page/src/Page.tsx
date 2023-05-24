efee5fdd50b8993cb6090c05f3e6ed9d7f7ace1a
Matteo Alessani <matteo@commercelayer.io>
2023-05-24 23:20:30 +0200

    feat: add commerce components

--- src/components/Page/src/Page.tsx
+++ src/components/Page/src/Page.tsx
@@ -1,16 +1,28 @@
+// Page.tsx
+import { CartLink } from "@commercelayer/react-components/orders/CartLink";
+import CommerceLayerAuth from "../../CommerceLayerAuth";
 import * as styles from "./Page.module.scss";
+import { Link } from "gatsby";
 
 export type PageProps = {
   children: React.ReactNode;
   title: React.ReactNode;
 };
 
+const marketNumber = 13502;
+
+const clientId = `${process.env.CLIENT_ID}`;
+const slug = `${process.env.SLUG}`;
+
 const Page = ({ children, title }: PageProps) => {
   return (
-    <main className={styles.page}>
-      <h1 className={styles.title}>{title}</h1>
-      <div>{children}</div>
-    </main>
+    <CommerceLayerAuth clientId={clientId} slug={slug} market={marketNumber}>
+      <main className={styles.page}>
+        <Link to="/">Home</Link> | <CartLink label="Cart" />
+        <h1 className={styles.title}>{title}</h1>
+        <div>{children}</div>
+      </main>
+    </CommerceLayerAuth>
   );
 };
 
--- src/templates/pokemon.tsx
+++ src/templates/pokemon.tsx
@@ -1,5 +1,12 @@
+// pokemon
 import { graphql, PageProps } from "gatsby";
 import Page from "../components/Page";
+import * as styles from "../components/Page/src/Page.module.scss";
+import { AddToCartButton } from "@commercelayer/react-components/orders/AddToCartButton";
+import { AvailabilityContainer } from "@commercelayer/react-components/skus/AvailabilityContainer";
+import { AvailabilityTemplate } from "@commercelayer/react-components/skus/AvailabilityTemplate";
+import { PricesContainer } from "@commercelayer/react-components/prices/PricesContainer";
+import { Price } from "@commercelayer/react-components/prices/Price";
 
 const PokemonPage = ({ data }: PageProps<Queries.ProductPageQuery>) => {
   const { productsJson: product } = data;
@@ -7,6 +14,24 @@ const PokemonPage = ({ data }: PageProps<Queries.ProductPageQuery>) => {
   return (
     <Page title={product?.name || "Name not found"}>
       <p>{product?.description || "Description not found"}</p>
+      <p>
+        <PricesContainer>
+          <Price
+            skuCode={product?.sku}
+            className={styles.px}
+            compareClassName={styles.compare}
+          />
+        </PricesContainer>
+      </p>
+      <p>
+        {product?.sku && (
+          <AvailabilityContainer skuCode={product?.sku}>
+            <AvailabilityTemplate className={styles.px} />
+          </AvailabilityContainer>
+        )}
+
+        <AddToCartButton quantity={"1"} skuCode={product?.sku!} />
+      </p>
     </Page>
   );
 };
@@ -17,6 +42,7 @@ export const query = graphql`
   query ProductPage($id: String!) {
     productsJson(id: { eq: $id }) {
       name
+      sku
       description
     }
   }
